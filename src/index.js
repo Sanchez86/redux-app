32dcdf1a575c23fc585e38735fd927e9b0e54971
Alexandr <0719118@gmail.com>
2021-05-28 11:37:39 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import './style.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+    switch(action.type){
+        case 'INC' :
+            return state + 1;
+        case 'DEC' :
+            return state - 1;
+        case 'RND' :
+            return state + action.value;
+        case 'REF' :
+            return state = 0;
+        default:
+            return state;
+    }
+};
+//функции креейторы
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', value});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc());
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec());
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random()*10);
+    store.dispatch(rnd(value));
+});
+
+document.getElementById('ref').addEventListener('click', () => {
+    store.dispatch({type: 'REF'});
+});
+
+const update = () => {
+    document.getElementById('counter').innerHTML = store.getState();
+};
+
+store.subscribe(() => {
+  update();
+  console.log(store.getState());
+});
